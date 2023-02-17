642a2d08b8ae02bc0c6ca9113f875a4222de4b4f
Eli Orians <eli.orians@gmail.com>
2023-02-16 20:24:47 -0500

    instagram api

--- appstate-club-xctf/src/pages/Join.js
+++ appstate-club-xctf/src/pages/Join.js
@@ -7,13 +7,13 @@ const Join = () => {
 
         <div class='membership'>
             <h1>Club Membership</h1>
-            <ul>
+            <ol class='membership'>
                 <li>Join the club discord <a href='https://discord.gg/9p5XsXPEjD'>here.</a></li>
                 <li><a href='https://docs.google.com/document/d/1rg-6Rhcxl71IsuerFQdbXKAobsOyiVmcnTlm0OhPz10/edit?usp=sharing'>This MIC form</a> must be printed and filled out in black ink and hand it to an officer before attending practice.</li>
                 <li>Once your form is processed, you'll be invited to the <a href='https://engage.appstate.edu/organization/appstateclubxctrack'>club's engage</a> by email. Be sure to accept this invite!</li>
                 <li>Club dues are $35 per academic year. Pay cash to an officer, or through the website below. </li>
                 <li>Sign up for practice or races! Links to practice and race sheets can be found on the Schedule page. </li>
-            </ul>
+            </ol>
 
         </div>
 
@@ -22,20 +22,21 @@ const Join = () => {
         </div>
 
         <div class='instagram-api'>
-            {/*instagram widget: https://apps.elfsight.com/ under eli.orians@gmail.com account, has limit of 200 views per month with free version*/}
-            <h2>Instagram</h2>
-            <iframe title='instagram' allowtransparency frameborder='0' height='500' width='100%' scrolling='no' src='https://sandbox.elfsightcdn.com/cdf0106a-f9d5-431e-8993-84f72a2890d4'></iframe>
+            {/*instagram widget: https://lightwidget.com/ under eli.orians@gmail.com account*/}
+            <h2>Follow us on Instagram</h2>
+            <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+            <iframe title='instagram' src="//lightwidget.com/widgets/c8b0c2d632b4562c83f1b365ee42d4de.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" width='100%' height='550px' frameborder='0' overflow='hidden'></iframe>
         </div>
         
         <div class='strava-api'>
-            <h2>Strava</h2>
-            <iframe title='strava-summary'   allowtransparency frameborder='0' height='160' width='300' scrolling='no' src='https://www.strava.com/clubs/552116/latest-rides/9f64a45cda2e8e81c61aa3ad4af060e9372043a8?show_rides=false'></iframe>
-            <iframe title='strava-activites' allowtransparency frameborder='0' height='454' width='100%' scrolling='no' src='https://www.strava.com/clubs/552116/latest-rides/9f64a45cda2e8e81c61aa3ad4af060e9372043a8?show_rides=true'></iframe>
+            <h2>Join our Strava Club</h2>
+            <iframe title='strava-summary' allowtransparency frameborder='0' height='160' width='300' scrolling='no' src='https://www.strava.com/clubs/552116/latest-rides/9f64a45cda2e8e81c61aa3ad4af060e9372043a8?show_rides=false'></iframe>
+            <iframe title='strava-feed' allowtransparency frameborder='0' height='550' width='100%' scrolling='no' src='https://www.strava.com/clubs/552116/latest-rides/9f64a45cda2e8e81c61aa3ad4af060e9372043a8?show_rides=true'></iframe>
         </div>
 
         <div class='resources'>
             <h1>Other Resources</h1>
-            <ul>
+            <ul class='resources'>
                 <li><a href='https://drive.google.com/drive/folders/1RVJ1oUVhk8th2MOK0yAqurr7LcPA_azW?usp=sharing'>Club Photo Drive</a></li>
                 <li><a href='https://docs.google.com/document/d/1X61mcZ-Qzb3UOtF9QNvy_FvNOf3Kknn0/edit?usp=sharing&ouid=104804195159131396145&rtpof=true&sd=true'>Our Running Locations</a></li>  {/*Improve this by creating a list of strava or garmin routes*/}
             </ul>
