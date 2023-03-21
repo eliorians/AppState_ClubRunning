e35a5d3d11a87f5c53ff02eadd1527bf52940e74
Eli Orians <eli.orians@gmail.com>
2023-03-21 09:50:03 -0400

    return from spring break

--- appstate-club-xctf/src/pages/Schedule.js
+++ appstate-club-xctf/src/pages/Schedule.js
@@ -1,7 +1,35 @@
 import React from "react";
 
 const Schedule = () => {
-    return <h1>Schedule</h1>;
+    return (
+        <div class='schedule-wrapper'>
+
+            <div class='practice-text'>
+                <p>signup for practice here. each sheet is a different week, be sure to select the correct one</p>
+            </div>
+
+            <div class='practice'>
+                *BUILD PRACTICE SIGNUP W/ PROTECTED CELL RANGES
+            </div>
+
+            <div class='xc-race-text'>
+                <p>see our xc schedule and signup for fall races here. you will need to fill out a NIRCA waiver once an officer has registered you.</p>
+            </div>
+
+            <div class='xc-race'>
+                *BUILD XC SIGNUP W/ PROTECTED CELL RANGES
+            </div>
+
+            <div class='track-race-text'>
+                <p>see our track schedule and signup for spring races here. you will need to fill out a NIRCA waiver once an officer has registered you.</p>
+            </div>
+
+            <div class='track-race'>
+                *BUILD TRACK SIGNUP W/ PROTECTED CELL RANGES
+            </div>
+
+        </div>
+    );
 };
 
 export default Schedule;
