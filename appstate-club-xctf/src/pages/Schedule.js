792c46a0932ca036ac2483b31640b629e9d5cd7d
Eli Orians <eli.orians@gmail.com>
2023-03-27 12:54:40 -0400

    schedule page, links on join page, starting faq content

--- appstate-club-xctf/src/pages/Schedule.js
+++ appstate-club-xctf/src/pages/Schedule.js
@@ -5,27 +5,29 @@ const Schedule = () => {
         <div class='schedule-wrapper'>
 
             <div class='practice-text'>
-                <p>signup for practice here. each sheet is a different week, be sure to select the correct one</p>
+                <h2>Practice & Social Schedule</h2>
+                <p>Signup for practice and social events with this sheet. Each sheet is a different week, so be sure to select the correct one. <a href='https://docs.google.com/spreadsheets/d/1wYgAsF7oTKeRjVKKrxVIs3S7ng8lQfiNRCADQbZ8EQM/edit?usp=sharing'>Click here</a> to view in Google Docs.</p>
+            </div>
+            <div class='practice'>
+                <center><iframe title='practice' height='1350' width='100%' src="https://docs.google.com/spreadsheets/d/1wYgAsF7oTKeRjVKKrxVIs3S7ng8lQfiNRCADQbZ8EQM/edit?usp=sharing?&rm=minimal&widget=true&headers=false" target='_blank' rel='noreferrer'></iframe></center>
             </div>
 
-            <div class='practice'>
-                *BUILD PRACTICE SIGNUP W/ PROTECTED CELL RANGES
-            </div>
 
             <div class='xc-race-text'>
-                <p>see our xc schedule and signup for fall races here. you will need to fill out a NIRCA waiver once an officer has registered you.</p>
+                <h2>Fall Race Signup (Cross Country)</h2>
+                <p>Below is our fall race schedule, each sheet is a different race. You will need to fill out a NIRCA waiver once an officer has registered you. <a href='https://docs.google.com/spreadsheets/d/1-XvodeBq4d7Vq41FoOiox2a3it-MYjp8FjdATjO4Pgk/edit?usp=sharing' target='_blank' rel='noreferrer'>Click here</a> to view in Google Docs.</p>
+            </div>
+            <div class='xc-race'>
+                <center><iframe title='xc-race' height='990' width='1100' src="https://docs.google.com/spreadsheets/d/1-XvodeBq4d7Vq41FoOiox2a3it-MYjp8FjdATjO4Pgk/edit?usp=sharing?&rm=minimal&widget=true&headers=false"></iframe></center>
             </div>
 
-            <div class='xc-race'>
-                *BUILD XC SIGNUP W/ PROTECTED CELL RANGES
-            </div>
 
             <div class='track-race-text'>
-                <p>see our track schedule and signup for spring races here. you will need to fill out a NIRCA waiver once an officer has registered you.</p>
+                <h2>Spring Race Signup (Track)</h2>
+                <p>Below is our spring race schedule, each sheet is a different race. Follow up with an officer after signing up. <a href='https://docs.google.com/spreadsheets/d/1Pdn4F8csvJFriQFSMZ6CXFiQaN6BXKtuT08MSrycqAI/edit?usp=sharing' target='_blank' rel='noreferrer'>Click here</a> to view in Google Docs.</p>
             </div>
-
             <div class='track-race'>
-                *BUILD TRACK SIGNUP W/ PROTECTED CELL RANGES
+                <center><iframe title='track-race' height='1090' width='100%' src="https://docs.google.com/spreadsheets/d/1Pdn4F8csvJFriQFSMZ6CXFiQaN6BXKtuT08MSrycqAI/edit?usp=sharing?&rm=minimal&widget=true&headers=false"></iframe></center>
             </div>
 
         </div>
