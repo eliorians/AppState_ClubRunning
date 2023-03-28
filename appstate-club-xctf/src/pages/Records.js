5d5afdaf55038eb235988eb4e832e9941fb45c1f
JosephMar808 <jmaradiaga901@gmail.com>
2023-03-28 16:24:25 -0400

    Records page is finally good to go

--- appstate-club-xctf/src/pages/Records.js
+++ appstate-club-xctf/src/pages/Records.js
@@ -7,7 +7,7 @@ const Records  = () => {
         <div class='records-wrapper'>
             <div class = 'background-image' style={{backgroundImage: `url(${background})`}}>
                 <div class = 'records'>
-                <center><iframe title='records' width='1150' height = "2190" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR9tXs_NmwLaS0QjaFrhc9c037Z6QmRN7KAJ5T6VpPS-2TO8szwNPbm9FBqOc3bdC2jG-w45Pm_2WgC/pubhtml?widget=true&amp;headers=false"></iframe></center>
+                    <center><iframe class='records' title='records' width='1150' height = "2190"  src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR9tXs_NmwLaS0QjaFrhc9c037Z6QmRN7KAJ5T6VpPS-2TO8szwNPbm9FBqOc3bdC2jG-w45Pm_2WgC/pubhtml?widget=true&amp;headers=false"></iframe></center>
                 </div>
             </div>
         </div>
