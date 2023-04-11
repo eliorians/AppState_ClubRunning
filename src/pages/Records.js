a44648db4ad72d4ea551927e50df29552cd974c2
Eli Orians <eli.orians@gmail.com>
2023-04-11 15:27:48 -0400

    more tweaks

--- src/pages/Records.js
+++ src/pages/Records.js
@@ -5,7 +5,7 @@ const Records  = () => {
     return (
         <div class='records-wrapper'>
             <div class = 'background-image' style={{backgroundImage: `url(${background})`}}>
-                <center><p class='button'><a class='button' href="https://docs.google.com/spreadsheets/d/1x7RUIj6KCTUKUYUShmq8UoEaAgyULV9Ewr9Iqh_H8p4/edit?usp=sharing"> Click here for a better view of the Records page if needed!!</a></p></center>
+                <center><p class='button'><a class='button' href="https://docs.google.com/spreadsheets/d/1x7RUIj6KCTUKUYUShmq8UoEaAgyULV9Ewr9Iqh_H8p4/edit?usp=sharing" target='_blank' rel='noreferrer'>Click here to view as Google Doc</a></p></center>
                 <div class = 'records'>
                     <center><iframe class='excel' title='records' width='1150' height = '2190' src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR9tXs_NmwLaS0QjaFrhc9c037Z6QmRN7KAJ5T6VpPS-2TO8szwNPbm9FBqOc3bdC2jG-w45Pm_2WgC/pubhtml?widget=true&amp;headers=false"></iframe></center>
                 </div>
